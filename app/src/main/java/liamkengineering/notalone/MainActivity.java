435a5983b48d631568d7478d9dc942ec6300d035
zappyfish <liamkelly2015@gmail.com>
2016-12-03 10:55:33 -0500

    setup

---
+++ app/src/main/java/liamkengineering/notalone/MainActivity.java
@@ -0,0 +1,26 @@
+package liamkengineering.notalone;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button settingsButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        settingsButton = (Button) findViewById(R.id.settings);
+        settingsButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent i = new Intent(MainActivity.this, Settings.class);
+                startActivity(i);
+            }
+        });
+    }
+}
