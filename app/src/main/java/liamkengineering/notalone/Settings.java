1821dbb7cafb75199a1919211ae89fb1cbcac41a
zappyfish <liamkelly2015@gmail.com>
2016-12-03 12:49:35 -0500

    photos

--- app/src/main/java/liamkengineering/notalone/Settings.java
+++ app/src/main/java/liamkengineering/notalone/Settings.java
@@ -1,11 +1,13 @@
 package liamkengineering.notalone;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,10 +18,15 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import java.io.FileNotFoundException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Settings extends AppCompatActivity {
     List<Uri> photoList;
@@ -29,6 +36,7 @@ public class Settings extends AppCompatActivity {
     public static final String PREFS_NAME = "NKDROID_APP";
     public static final String PHOTOS = "Photo";
     SharedPreferences sharedPrefs;
+    Context ctx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +44,13 @@ public class Settings extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        sharedPrefs = getSharedPreferences("App_settings", Context.MODE_PRIVATE);
         Button buttonLoadImage = (Button) findViewById(R.id.loadimage);
         Button nextButton = (Button) findViewById(R.id.next);
+        Button saveButton = (Button) findViewById(R.id.save);
         textTargetUri = (TextView) findViewById(R.id.targeturi);
         targetImage = (ImageView) findViewById(R.id.targetimage);
-
+        ctx = this;
         buttonLoadImage.setOnClickListener(new Button.OnClickListener() {
 
             @Override
@@ -51,6 +61,12 @@ public class Settings extends AppCompatActivity {
                 startActivityForResult(intent, 0);
             }
         });
+        saveButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                //saveToInternalStorage();
+                savePhotos(ctx);
+            }});
         nextButton.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View arg0) {
@@ -73,7 +89,7 @@ public class Settings extends AppCompatActivity {
                 }
             }
         });
-        photoList = readFromInternalStorage();
+        photoList = readFromInternalStorage(ctx);
         if(photoList == null) {
             photoList = new ArrayList<>();
         }
@@ -96,6 +112,33 @@ public class Settings extends AppCompatActivity {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
+        }
+    }
+    public void savePhotos(Context ctx) {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        Set<String> s = new HashSet<String>();
+        for(int i =0; i<photoList.size();i++) {
+            s.add(photoList.get(i).toString());
+        }
+        editor.putStringSet("PHOTOS", s);
+        editor.apply();
+
+    }
+
+    public List<Uri> readFromInternalStorage(Context ctx) {
+        List<Uri> l = new ArrayList<>();
+        List<String> temp = new ArrayList<>();
+        Set<String> s = sharedPrefs.getStringSet("PHOTOS", null);
+
+        if(s!= null) {
+            temp.addAll(s);
+            for (int i = 0; i < temp.size(); i++) {
+                l.add(Uri.parse(temp.get(i)));
+            }
+            return l;
+        }
+        else {
+            return null;
         }
     }
 
