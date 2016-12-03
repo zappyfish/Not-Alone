88167a4b6c74fd514697b7b2ebfd0e2162d426f2
Katherine Wu <KWU@dhcp-130-58-201-62.swarthmore.edu>
2016-12-03 15:50:32 -0500

    hi

---
+++ app/src/main/java/liamkengineering/notalone/Hotline.java
@@ -0,0 +1,8 @@
+package liamkengineering.notalone;
+
+/**
+ * Created by KWU on 12/3/16.
+ */
+
+public class Hotline {
+}
--- app/src/main/java/liamkengineering/notalone/MainActivity.java
+++ app/src/main/java/liamkengineering/notalone/MainActivity.java
@@ -3,6 +3,7 @@ package liamkengineering.notalone;
 NOTE: DON'T FORGET TO ENABLE STORAGE PERMISSINOS OR THE APP WILL FAIL
  */
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,8 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     Button settingsButton;
     Button textMessageButton;
+    Button hotlineButton;
+    String hotline = "8002738255";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,15 @@ public class MainActivity extends AppCompatActivity {
                 Intent i = new Intent(MainActivity.this, TextMessage.class);
                 startActivity(i);
 
+            }
+        });
+
+        hotlineButton = (Button) findViewById(R.id.hotline);
+        hotlineButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent i = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + hotline));
+                startActivity(i);
             }
         });
     }
